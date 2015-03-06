279b47260749bb3bdb63746b5b0deef5abff9ab7
Zhiting Zhu <zzt0215@uw.edu>
2015-03-06 00:47:53 -0600

    seems get a correct answer; need to generalized to any # of threads for any size array

--- hw2/p2/genscan.c
+++ hw2/p2/genscan.c
@@ -13,45 +13,33 @@ void genericScan(void *X, int n, size_t l)
   int tid;
   int offset = 1;
   int step = (int)ceil(log2(n));
-  printf("step: %d\n", step);
   int nthreads;
 #pragma omp parallel private(tid, i, nthreads, offset) shared(c, step, n, l) 
   {
     offset = 1;
     tid = omp_get_thread_num();
-    if (tid == 0) {
-      nthreads = omp_get_num_threads();
-      printf("num threads: %d\n", nthreads);
-    }
     int temp = n >> 1;
-#pragma omp parallel for 
     for (i = step; i>0; i--) {
-      printf("First for loop: %d\n", tid);
+      #pragma omp barrier
       if (tid < temp) {
-        printf("access: %d, %d\n", offset*(2*tid+1)-1, offset*(2*tid+2)-1);
         ubop(c+(offset*(2*tid+2)-1)*l, c+(offset*(2*tid+1)-1)*l);
-        printf("%f, %f\n", ((double*)c)[(offset*(2*tid+2)-1)], ((double*)c)[(offset*(2*tid+1)-1)]);
       }
-      printf("offset, temp: %d, %d\n", offset, temp);
       offset *= 2;
       temp = temp >> 1;
     }
-/* #pragma omp barrier */
-/*     temp  = 1; */
-/*     offset >>= 1; */
-/* #pragma omp parallel for */
-/*     for (i = 1; i < step; i ++) { */
-/*       offset >>= 1; */
-/*       printf("Second for loop: %d\n", tid); */
-/*       printf("offset, temp: %d, %d\n", offset, temp); */
-/*       if (tid <= temp && tid > 0) { */
-/*         printf("access: %d, %d\n", offset*(2*tid+1)-1, offset*(2*(tid-1)+2)-1); */
-/*         if (offset*(2*tid+1)-1 < n && offset*(2*(tid-1)+2)-1 < n) */
-/*           ubop(c+(offset*(2*tid+1)-1)*l, c+(offset*(2*(tid-1)+2)-1)*l); */
-/*       } */
-/*       temp *= 2; */
-
-/*     } */
+    temp  = 2;
+    offset >>= 1;
+    for (i = 1; i < step; i ++) {
+      #pragma omp barrier
+      offset >>= 1;
+      if (tid < temp && tid > 0) {
+        int a = offset*(2*tid+1)-1;
+        int b = offset*(2*tid)-1;
+        if ( a < n &&  b < n)
+          ubop(c+a*l, c+b*l);
+      }
+      temp *= 2;
+    }
   }
 }
 
