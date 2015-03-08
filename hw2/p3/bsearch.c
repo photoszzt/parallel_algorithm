8cccfae54e85bd1f7b828712f8dde2e8decc8b5a
Zhiting Zhu <zzt0215@uw.edu>
2015-03-08 18:15:10 -0500

    mpi send and recv data finish, need to work on bsearch

--- hw2/p3/bsearch.c
+++ hw2/p3/bsearch.c
@@ -1,12 +1,69 @@
 #include <mpi.h>
 #include <omp.h>
+#include <algorithm>
+#include <time.h>
+#include <set>
+#include<stdlib.h>
 
-int bsearch(MPI_Comm comm, int* keys, int num_keys, int* arr, int arr_size, int num_threads) {
-  int rank, size;
-  MPI_Comm_size(comm, &size);
-  MPI_Comm_rank(comm, &rank);
-  int len = arr_size/size;
-  int low = len*rank;
-  int high = low+len;
-  
+int init(MPI_Comm comm, int* arr, int arr_size, int low) {
+  srand(time(NULL));
+  std::set<int> numbers_gen;
+  for (int i = 0; i < arr_size; i++) {
+    int new_val = rand() % arr_size + low;
+    while (numbers_gen.find(new_val) != numbers_gen.end())
+      new_val = rand() % arr_size + low;
+    arr[i] = new_val;
+    numbers_gen.insert(new_val);
+  } 
+  sort(arr, arr+arr_size);
+}
+
+int init_and_bsearch(MPI_Comm comm, int* keys, int num_keys, int* arr, int arr_size, int num_ts, int rank, int** pos) {
+  int low = arr_size*rank;
+  int high = low+arr_size;
+  init(comm, arr, arr_size, low);
+  int tid; 
+  int i;
+  int nthreads;
+  int send_buf[2][2];
+  int recv_buf[2];
+  for (i = 0; i < 2; i++) {
+    memset(send_buf[i], 0, sizeof(int)*2);
+  }
+  memset(recv_buf, 0, sizeof(int)*2);
+  for (i = 0; i < num_keys; i++ ) {
+    int k = keys[i];
+    if (k < arr[high-1] && k > arr[low]) {
+      int position = 0;
+#pragma omp parallel shared(keys, num_keys, arr, arr_size, len, low, high, k, position) private(tid, i, nthreads) num_threads(num_ts) 
+      {
+        tid = omp_get_thread_num();
+        nthreads = omp_get_num_threads();
+        
+      }
+      send_buf[0][0] = position;
+      send_buf[0][1] = i;
+      MPI_Request req;
+      MPI_Status status;
+      MPI_Isend(send_buf[0], 2, MPI_INT, 0, 2, comm, &req);
+      MPI_Wait(&req, &status);
+    } else if ((k == arr[high-1]) || (k == arr[low])) {
+      if (k == arr[high-1])
+        send_buf[1][0] = high-1;
+      else
+        send_buf[1][0] = low;
+      send_buf[1][1] = i;
+      MPI_Request req;
+      MPI_Status status;
+      MPI_Isend(send_buf[1], 2, MPI_INT, 0, 1, comm, &req);
+      MPI_Wait(&req, &status);
+    } 
+    if (rank == 0) {
+      MPI_Request req;
+      MPI_Status status;
+      MPI_Irecv(recv_buf, 2, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+      MPI_Wait(&req, &status);
+      (*pos)[recv_buf[1]] = recv_buf[0];
+    }
+  }
 }
--- hw2/p3/search.c
+++ hw2/p3/search.c
@@ -2,41 +2,35 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <math.h>
-#include <algorithm>
 #include <time.h>
-#include <set>
 #include "bsearch.h"
 
-
-int init_arr(MPI_Comm comm, int* arr, int arr_size) {
-  int rank, size;
-  MPI_Comm_size(comm, &size);
-  MPI_Comm_rank(comm, &rank);
-  int len = arr_size / size;
-  int low = len*rank;
-  int high = low + len;
-  srand(time(NULL));
-  std::set<int> numbers_gen;
-  for (int i = low; i < high; i++) {
-    int new_val = rand() % len + low;
-    while (numbers_gen.find(new_val) != numbers_gen.end())
-      new_val = rand() % len + low;
-    arr[i] = arr[i];
-  } 
-  sort(arr+low, arr+high);
-}
-
 int main(int argc, char** argv) {
- if (argc != 2) {
+  if (argc != 2) {
     fprintf(stderr, "Usage: %s <size_of_array>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
   int arr_size = atoi(argv[1]);
   MPI_Comm comm = MPI_COMM_WORLD;
-  int * arr = (int*) malloc(size_of_arr*sizeof(int));
-  MPI_Init(&argc, &argv);
-  init_arr(comm, arr, arr_size);
-  bsearch(comm, keys, NUM_KEYS, arr, arr_size, NUM_THREADS);
+  MPI_Init(NULL, NULL);
+  int rank, size;
+  MPI_Comm_size(comm, &size);
+  MPI_Comm_rank(comm, &rank);
+  int * arr = NULL;
+  int * keys = NULL;
+  if (rank == 0) {
+    arr = (int*) malloc(arr_size*sizeof(int));
+    keys = (int*) malloc(sizeof(int)*NUM_KEYS);
+    srand(time(NULL));
+    for (int i = 0; i < NUM_KEYS; i++) {
+      keys[i] = rand() % arr_size;
+    }
+  }
+  int len = arr_size / size;
+  int * sub_arr = (int*) malloc(sizeof(int)*len);
+  MPI_Scatter(arr, len, MPI_INT, sub_arr, len, MPI_INT, 0, comm);
+  MPI_Bcast(keys, NUM_KEYS, MPI_INT, 0, comm);
+  init_and_bsearch(comm, keys, NUM_KEYS, sub_arr, len, NUM_THREADS, rank);
   MPI_Finalize();
   return 0;
 }
