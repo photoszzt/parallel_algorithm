3612f56b92bc2cd1bbd4014aa6b4120ef2200098
Zhiting Zhu <zzt0215@uw.edu>
2015-03-08 21:29:07 -0500

    forget h file

---
+++ hw2/p3/bsearch.h
@@ -0,0 +1,9 @@
+#ifndef __BSEARCH_H__
+#define __BSEARCH_H__
+
+#define NUM_KEYS (0x1 << 20)
+#define NUM_THREADS 6
+
+int init_and_bsearch(MPI_Comm comm, int* keys, int num_keys, int* arr, int arr_size, int num_ts, int rank, int** pos);
+
+#endif // __BSEARCH_H__
